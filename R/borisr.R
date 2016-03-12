95a34ae54ed3c0c019626d855926b69ace33c427
Pat Bills <pat@billspat.com>
2016-03-12 17:22:13 -0500

    initial package commit

---
+++ R/borisr.R
@@ -0,0 +1,61 @@
+
+read_boris <- function(boris_file) {
+  b = readLines(boris_file,  warn = "F")
+  dat = fromJSON(b)
+  return(dat)
+}
+
+get_obslist<- function(dat){
+  # check if dat is boris data
+  names(dat$observations)
+}
+
+get_events <- function(dat,obs_number) {
+  # extract the events to a data frame for this one trial by trial name or index number
+  # to do: add a column of the obs_number for later grouping
+  # to do: add time offset to all times?
+  obs = dat$observations[[obs_number]]
+  group_column = rep(obs_number,nrow(obs$events))
+
+  obs.df = data.frame(group_column, obs$events, stringsAsFactors = FALSE)
+
+  names(obs.df) = c("obs_id", "time","ID","event","subevent","unknown")
+  obs.df = split_subevents(obs.df)
+  return(obs.df)
+}
+
+split_subevents <-function(evdata, seperator = "|", cnames = c("subev1", "subev2")) {
+  # yikes!  spliting a column of strings is not simple
+  # to do : make this more generic for any column, not just subevents
+  subevents = evdata$subevent
+
+  # first have to split the strings, but get a list of vectors back
+  # have to use the "fixed" function because if seperator is a regular expression special
+  #   character, like |, need to ignore that
+  subev.list = str_split(subevents, fixed(seperator))
+
+  # function to make two strings out of one or two
+  # these data sometimes have two, and sometimes one with no seperator
+  make_two <- function(v){
+    if (length(v) < 2) {v[2] = ""}
+    return(c(v[1],v[2]))
+  }
+
+  # apply function above to entire list
+  subev.list = lapply(subevlist,make_two)
+
+  # convert list of vectors to two column matrix
+  subev.matrix = do.call(rbind, lapply(subevlist, rbind))
+
+  # add names from the function arguments to these two columns
+  colnames(subev.matrix) = cnames
+
+  # convert THAT To the data frame of two columns
+  subev.df = data.frame(subev.matrix, stringsAsFactors = FALSE)
+
+  # add these two columns to original data
+  return(data.frame(evdata,subev.df))
+
+}
+
+

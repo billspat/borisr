c9b35f09fd1ce325e0d7895fb95e0404b34dd4ba
Pat Bills <pat@billspat.com>
2016-03-24 22:01:44 -0400

    more testing

--- tests/testthat/test-openfile.R
+++ tests/testthat/test-openfile.R
@@ -1,14 +1,27 @@
 library(testthat)
 library(jsonlite)
+library(borisr)
 
 context("testing  boris")
-# should test this for ALL behaviors - in a loop?
-boris_file = '../../data/examplevideotrial.boris'
 
+# TO DO:
+boris_file = '../../data/examplevideotrial.boris'
+boris.data = borisr::read_boris(boris_file)
+test_obs_names = c("animalu22","animalu41")  # to do, make it generic
 
 test_that("can open a boris file", {
-  boris.data = read_boris(boris_file)
   expect_gt(length(boris.data),0)
 })
 
+test_that("can read boris data structure ", {
+  expect_gt(length(names(boris.data)),0)
+  expect_true("observations" %in% names(dat))
+})
+
+test_that("can get obs ",{
+  obs = get_obslist(boris.data)
+  expect_gt(length(obs),0)
+  for(ob in test_obs_names) {expect_true(ob %in% obs)}
+
+})
 
